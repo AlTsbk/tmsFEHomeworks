e1522cc5083ea17a0317971cace51a6467f61893
Yana Petrovskaya <105793821+petrovskya@users.noreply.github.com>
2022-07-27 17:55:54 +0300

    add hw-2 (#6)

---
+++ Yana_Petrovskaya/hw2/index.js
@@ -0,0 +1,97 @@
+// Task 1.
+
+// let x = 20;
+// let y = 58;
+// let z = 42;
+
+// alert('Дано: x=' + x + ', y=' + y + ', z=' + z + '. Найдите сумму данных чисел.');
+// alert('Cумма чисел: ' + (x + y + z) + '.');
+
+// Task 2.
+
+// let nSecond = 60;
+// let nMinute = 60;
+// let nHour = 24;
+// let nDay = 365;
+// let myAgeInSeconds = +prompt('Введите Ваш возраст, пожалуйста!');
+
+// alert('Ваш возраст в днях равен: ' + (myAgeInSeconds*nDay) + ' дн.');
+// alert('Ваш возраст в часах равен: ' + (myAgeInSeconds*nDay*nHour) + ' ч.');
+// alert('Ваш возраст в минутах равен: ' + (myAgeInSeconds*nDay*nHour*nMinute) + ' мин.');
+// alert('Ваш возраст в секундах равен: ' + (myAgeInSeconds*nDay*nHour*nMinute*nSecond) + ' c.');
+
+// Task 3.
+
+// let count = 42;
+// let userName = '42';
+
+// let inNumber = Number(console.log(userName));
+// let inString = String(console.log(count));
+
+// let count = String(42);
+// let userName = +'42';
+
+// Task 4.
+
+// let a = 1;
+// let b = 2;
+// let c = 'белых медведей';
+
+// alert(String(a) + String(b) + ' ' + c);
+
+// Task 5.
+
+// let a = 'доступ';
+// let b = 'морпех';
+// let c = 'наледь';
+// let d = 'попрек';
+// let e = 'рубило';
+
+// let lengthWords = a.length + b.length + c.length + d.length + e.length;
+// alert('Длина всех слов равна: ' + lengthWords +'.');
+
+// Task 6.
+
+// let sFirstVariable = 'Привет';
+// let nSecondVariable = 22;
+// let bThirdVariable = true;
+
+// alert('Variable: ' + String(Object.keys({sFirstVariable}))  + ' have type: ' + String(typeof(sFirstVariable)));
+// alert('Variable: ' + String(Object.keys({nSecondVariable}))  + ' have type: ' + String(typeof(nSecondVariable)));
+// alert('Variable: ' + String(Object.keys({bThirdVariable}))  + ' have type: ' + String(typeof(bThirdVariable)));
+
+// Task 7.
+
+// let sName = prompt('Введите Ваше имя!');
+// let nAge = Number(prompt('Введите Ваш возраст!'));
+// while(!nAge) {
+//   nAge =+prompt('Введите Ваш возраст!');
+// }
+// alert('Ваше имя: ' + sName + '. ' + 'Ваш возраст: ' + nAge + '.');
+
+
+// Advanced Level 
+
+// Task 1.
+
+// let a = 4;
+// let b = 3;
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// alert('a = ' + a);
+// alert('b = ' + b);
+
+// Task 2.
+
+// let codeWord1 = 'обернись';
+// let codeWord2 = 'неужели';
+// let codeWord3 = 'огурцы';
+// let codeWord4 = 'липкие';
+// let codeWord5 = '?!';
+
+// let cipher = alert(codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1]);
+
+
