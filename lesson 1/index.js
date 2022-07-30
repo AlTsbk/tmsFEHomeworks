2f80b177321bf46135c5098064d1c102ab8229e5
Dezet36 <sadkovsky36@gmail.com>
2022-07-30 09:54:07 +0300

    Lesson 1 full

---
+++ lesson 1/index.js
@@ -0,0 +1,92 @@
+//task 1
+
+let x = 20;
+let y = 58;
+let z = 42;
+
+console.log(x+y+z);
+
+// Task 2
+
+let age = +prompt ("Введите вашу возраст");
+let secont = 60;
+let minute = 60;
+let huor = 24;
+let days = 365;
+
+alert((days * age) * huor * minute * secont);
+
+// Task 3
+
+let count = 42;
+let userName = 42;
+
+
+let scount = String(42);
+let nuserName = +'42';
+
+let nCount = console.log(Number.(userName));
+let suserName = console.log(String.(count));
+
+
+// Task 4
+
+let a = 1
+let b = 2
+let c = "белых медведей"
+
+console.log ( String(a) + String(b) + " " + c);
+
+// Task 5
+
+
+let a = "доступ";
+let b = "морпех";
+let c = "наледь";
+let d = "попрек";
+let e = "рубило";
+
+console.log (a.length + b.length + c.length + d.length + e.length);
+
+// Task 6
+
+let a = 20;
+let b = '20';
+let c = true;
+console.log ('Variable: ' + String(Object.keys({a}))  + ' have type: ' + String(typeof(a)));
+console.log ('Variable: ' + String(Object.keys({b}))  + ' have type: ' + String(typeof(b)));
+console.log ('Variable: ' + String(Object.keys({c}))  + ' have type: ' + String(typeof(c)));
+
+// // Task 7
+
+let Uname = prompt ("Введите ваше имя");
+let Uage = +prompt ("Введите ваш возраст");
+
+console.log ("Имя вользователя: " + Uname);
+console.log ("Возвраст пользователя: " + Uage);
+
+// Task 1 Advanced
+
+let a = 4
+let b = 3
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+console.log (a);
+
+
+// Task 2
+
+let codeWord1 = "обернись";
+let codeWord2 = "неужели";
+let codeWord3 = "огурцы";
+let codeWord4 = "липкие";
+let codeWord5 = "?!";
+
+let cipher = (codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1]);
+
+console.log (cipher);
+
+
