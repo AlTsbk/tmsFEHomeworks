1b8969f93bea3c90d927a15452fb12792009afc0
Alexey Tsyabuk <42218271+AlTsbk@users.noreply.github.com>
2022-08-03 18:38:33 +0400

    arrays lesson example

---
+++ lessons-examples/arrays.js
@@ -0,0 +1,97 @@
+// function hi(){
+//     console.log("hi");
+// }
+
+// function bye(){
+//     console.log("bye");
+// }
+
+// function func(bParam, func1, func2){
+//     if(bParam){
+//         func1();
+//     }else{
+//         func2();
+//     }
+// }
+
+// console.log(hi, bye);
+
+// func(false, hi, bye);
+
+let aArr = [1,2,3,4,"sdasd",{},[],null, true];
+
+// let aArr2 = [1,2,3,4,"sdasd",{},[],null, true];
+
+// console.log(aArr[1]);
+
+// console.log(aArr.length)
+
+// aArr[99] = "hi";
+
+ let aConstrucorArr = new Array(10);
+
+// let aArr = [3];
+
+// console.log(aConstrucorArr);
+
+// aArr[2] = "bye";
+
+// console.log(aArr);
+
+// console.log(aArr === aArr2);
+
+// aArr.push("1");
+
+// console.log(aArr);
+
+// console.log(aArr.pop());
+
+// aArr.unshift("lol");
+
+// console.log(aArr);
+
+// aArr.shift()
+
+// console.log(aArr);
+
+// aArr[aArr.length] = "100"
+
+// aArr.length = 4;
+
+// 
+
+// aArr.splice(4,3,5);
+
+// console.log(aArr);
+
+// for(let i = 0; i < aArr.length; i++){
+//     console.log(aArr[i]);
+// }
+
+// for (const value of aArr) {
+//     console.log(value);
+// }
+
+// console.log(aArr);
+
+// aArr.reverse();
+
+// let aNewArray = aArr.concat([1,2,3,4]);
+
+// console.log(aArr.indexOf("sdasds"));
+
+// console.log(aArr.slice(2,4))
+
+// console.log(aArr.includes(1444));
+
+// let aGoodsList = ["молоко", "хлеб", "колбаса"];
+
+// console.log(aGoodsList.join("; "));
+
+// let aMatrix = [[1,4,5],[2,6,7],[3,8,9]];
+
+// console.log(aMatrix);
+// console.log(aMatrix.flat());
+
+
+// console.log(aConstrucorArr.fill("0"));
