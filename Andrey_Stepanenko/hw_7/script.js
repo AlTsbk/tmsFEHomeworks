7c7b65a7e3f9351a1f5158fd9916572b1ad13671
andronj <andstepanenko@tut.by>
2022-09-04 13:19:11 +0300

    add 2 task advanced

--- Andrey_Stepanenko/hw_7/script.js
+++ Andrey_Stepanenko/hw_7/script.js
@@ -52,3 +52,50 @@
 
 // const findNumber = numbers.find((item) => item % 2 ===0);
 // console.log(findNumber);
+
+// ### ADVANCED level
+
+// #### Task 1 👨‍🏫
+
+// function Student (name, salary, rate){
+//     this.name = name;
+//     this.salary = salary;
+//     this.rate = rate;
+// }
+
+// let user = new Student('Alex', 100, 'A');
+// let user2 = new Student('Katerina', 250, 'B')
+// let user3 = new Student('Andrey', 200, 'A');
+// let user4 = new Student('Igor', 20, 'D');
+// let user5 = new Student('Sofia', 300, 'C');
+
+// console.log(user, user2, user3, user4, user5);
+
+// #### Task 2 👨‍🏫 Тролли атакуют наш раздел с комментариями!!!
+
+// let disemvowel = (str) => str.replace(/[aeiouy]/gi, '')
+// let trollcomment = prompt('Введите строку')
+// console.log(disemvowel(trollcomment));
+
+
+
+// #### Task 4
+    let a = ("1 2 3 4 5"); 
+    let b = ("1 2 -3 4 5"); 
+    let c = ("1 9 3 4 -5");
+
+    let one = a.split(' ');
+    let m = Math.max(...one);
+    let min = Math.min(...one);
+    console.log(m, min);
+
+    let two = b.split(' ');
+    let n = Math.max(...two);
+    let o = Math.min(...two);
+    console.log(n, o);
+
+    let three = c.split(' ');
+    let k = Math.max(...three);
+    let l = Math.min(...three);
+    console.log(k, l);
+
