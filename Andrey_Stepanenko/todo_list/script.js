c4689af000d5dbc2afada6096f8e59911875d748
andronj <andstepanenko@tut.by>
2022-09-12 21:57:21 +0300

    add localstorage

--- Andrey_Stepanenko/todo_list/script.js
+++ Andrey_Stepanenko/todo_list/script.js
@@ -1,3 +1,18 @@
+
+let aArrOfTasks = [];
+let input = document.getElementById('myInput');
+
+window.addEventListener('load', () => {
+    aArrOfTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    
+        aArrOfTasks.forEach(oTask => {
+            renderElement(oTask);
+        })
+    
+
+})
+
+
 //Создаем у всех li кнопки close (крестик)
 let myNodelist = document.getElementsByTagName('li');
 for (let i = 0; i < myNodelist.length; i++) {
@@ -11,32 +26,70 @@ let list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
     if(ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
+        aArrOfTasks.find((oTask) => {
+           return oTask.id == ev.target.id
+        }).isDone = ev.target.classList.contains('checked')
+        console.log(aArrOfTasks);
+        localStorage.setItem('tasks', JSON.stringify(aArrOfTasks));
     }
 })
-//Создание нового элемента из данных в myInput
-function newElement() {
-    let li = document.createElement('li');
-    let inputValue = document.getElementById('myInput').value;
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if(inputValue === ''){
-        alert('Поле не может быть пустым!');
-    } else {
-        document.getElementById('myUL').appendChild(li);
-    }
 
+function renderElement(oTask) {
+    let li = document.createElement('li');
+    let t = document.createTextNode(oTask.text);
+    li.appendChild(t);
+    li.id = oTask.id
+    let dateElement = document.createElement('input');
+    dateElement.type = 'date';
+    dateElement.valueAsDate = new Date(oTask.date);
+    dateElement.addEventListener('change', () => {
+        aArrOfTasks.find((oTask) => {
+            return oTask.id == li.id
+         }).date = dateElement.valueAsDate;
+         localStorage.setItem('tasks', JSON.stringify(aArrOfTasks));
+    })
+    li.appendChild(dateElement);
+    if(oTask.isDone){
+        li.classList.add('checked')
+
+    }
     //Делаем кнопку для удаления (крестик)
     createCloseButton(li);
 
-    //Чистим поле input
-    document.getElementById('myInput').value = "";
+    
+    document.getElementById('myUL').appendChild(li);
 
+}
+
+//Создание нового элемента из данных в myInput
+function newElement() {
+    if(input.value === ''){
+        alert('Поле не может быть пустым!');
+    } else {
+       
+    
+    let oNewTask = {
+        id: new Date() * 2,
+        date: new Date(),
+        text: input.value,
+        isDone: false
+    }
+    aArrOfTasks.push(oNewTask);
+    localStorage.setItem('tasks', JSON.stringify(aArrOfTasks));
+    renderElement(oNewTask);
+     //Чистим поле input
+     input.value = "";
+    }
+   
+    
     //Делаем кнопку для удаления delete last
     // lastbt(li);
 }
     //делаем кнопки delete all и delete last
 let deleteLastBtn = document.querySelector('.deleteLastBtn');
 let deleteAllbtn = document.querySelector('.deleteAllBtn');
+// let showAllBtn = document.querySelector('.showAllBtn');
+// let showCompletedBtn = document.querySelector('.showCompletedBtn');
 
 let task = document.querySelector('.spisok')
 let lastChild = task.lastElementChild;
@@ -45,12 +98,28 @@ function myFunction() {
    return task.innerHTML = ''; 
 }
 function lastbt() {
-    return lastChild.style.display = 'none';
+     lastChild.remove();
 }
 
 
 deleteAllbtn.addEventListener('click', myFunction);
 deleteLastBtn.addEventListener('click', lastbt);
+// showCompletedBtn.addEventListener("click", ()=>{
+//     let task = document.querySelector('.spisok')
+//     let list = document.querySelectorAll('li');
+//     task.forEach(df => {
+//       if(!list.classList.contains("checked")){
+//         list.style.display = "none";
+//       }
+//     });
+//   });
+ 
+//   let li = document.querySelectorAll('li')
+//   showAllBtn.addEventListener("click", ()=>{
+//     task.forEach(li => {
+//       li.style.display = "block";
+//     });
+//   });
 
 
 function createCloseButton(myNodelist) {
@@ -61,7 +130,9 @@ function createCloseButton(myNodelist) {
     myNodelist.appendChild(span);
     span.onclick = function() {
             let div = this.parentElement;
-            div.style.display = "none";
+            div.remove();
+            aArrOfTasks.splice(myNodelist.id, 1);
+            localStorage.setItem('tasks', JSON.stringify(aArrOfTasks));
         }
 }
 // Добавляем эластичный поиск по элементам списка
